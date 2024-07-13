bc74b76f73c0cab6a4780c4c71488dd48adef9ed
Shayerine <lordacaelusmuintir@hotmail.com>
2024-07-13 01:28:43 -0500

    adding items with placeholder sprites (courtesy of Kaupenjoe)

--- src/main/java/net/donut/dexrewards/item/ModItems.java
+++ src/main/java/net/donut/dexrewards/item/ModItems.java
@@ -12,9 +12,27 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
     public static final Item DEXREWARD1 = registerItem("dexreward1", new Item(new FabricItemSettings()));
+    public static final Item DEXREWARD2 = registerItem("dexreward2", new Item(new FabricItemSettings()));
+    public static final Item DEXREWARD3 = registerItem("dexreward3", new Item(new FabricItemSettings()));
+    public static final Item DEXREWARD4 = registerItem("dexreward4", new Item(new FabricItemSettings()));
+    public static final Item DEXREWARD5 = registerItem("dexreward5", new Item(new FabricItemSettings()));
+    public static final Item DEXREWARD6 = registerItem("dexreward6", new Item(new FabricItemSettings()));
+    public static final Item DEXREWARD7 = registerItem("dexreward7", new Item(new FabricItemSettings()));
+    public static final Item DEXREWARD8 = registerItem("dexreward8", new Item(new FabricItemSettings()));
+    public static final Item DEXREWARD9 = registerItem("dexreward9", new Item(new FabricItemSettings()));
+    public static final Item DEXREWARD10 = registerItem("dexreward10", new Item(new FabricItemSettings()));
 
     public static void addToCreativeTabGroup(FabricItemGroupEntries entries){
         entries.add(DEXREWARD1);
+        entries.add(DEXREWARD2);
+        entries.add(DEXREWARD3);
+        entries.add(DEXREWARD4);
+        entries.add(DEXREWARD5);
+        entries.add(DEXREWARD6);
+        entries.add(DEXREWARD7);
+        entries.add(DEXREWARD8);
+        entries.add(DEXREWARD9);
+        entries.add(DEXREWARD10);
     }
 
     public static Item registerItem(String name, Item item){
